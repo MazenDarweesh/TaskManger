8392d2a7333b608920d114217a2e0a42c4f315d2
MazenDarweesh <mazentharwatfathy@gmail.com>
2024-08-27 12:43:41 +0300

    removing data annotations

--- Domain/Entities/TaskDomain.cs
+++ Domain/Entities/TaskDomain.cs
@@ -5,23 +5,14 @@ namespace Domain.Models;
 
 public class TaskDomain
 {
-    //prevent a property from being serialized into JSON.
-    [JsonIgnore]
     public Ulid Id { get; set; }
-
-    [Required]
-    [StringLength(100)]
     public required string Title { get; set; }
-
-    [StringLength(500)]
     public required string Description { get; set; }
     public bool IsCompleted { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-
     // Foreign key to the Student entity
     public Ulid StudentId { get; set; }
-
     // Navigation property  allows you to navigate from a task to the related student
     public Student Student { get; set; }
 }
