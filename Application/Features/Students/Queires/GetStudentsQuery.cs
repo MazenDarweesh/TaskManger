2c70017a96bac0cb97423d9381e8fc22d53b9ee5
MazenDarweesh <mazentharwatfathy@gmail.com>
2024-09-01 14:52:06 +0300

    edit handlersutdent

--- Application/Features/Students/Queires/GetStudentsQuery.cs
+++ Application/Features/Students/Queires/GetStudentsQuery.cs
@@ -1,8 +1,17 @@
 using MediatR;
 using Application.DTOs;
 using Application.Models;
+using Application.IServices;
 
 public class GetStudentsQuery : IRequest<PagedList<StudentDTO>>
 {
     public PaginationParams PaginationParams { get; set; }
+}
+public class GetStudentsQueryHandler(IStudentService studentService) : IRequestHandler<GetStudentsQuery, PagedList<StudentDTO>>
+{
+    private readonly IStudentService _studentService;
+    public async Task<PagedList<StudentDTO>> Handle(GetStudentsQuery request, CancellationToken cancellationToken)
+    {
+        return await _studentService.GetAllStudentsAsync(request.PaginationParams);
+    }
 }
