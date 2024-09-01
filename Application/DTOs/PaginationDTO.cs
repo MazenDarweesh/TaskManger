99804e734a7b7825d451c4a9b486439809a646a1
MazenDarweesh <mazentharwatfathy@gmail.com>
2024-09-01 13:38:20 +0300

    try add pagination validation

---
+++ Application/DTOs/PaginationDTO.cs
@@ -0,0 +1,8 @@
+//namespace Application.DTOs
+//{
+//    public class PaginationDTO
+//    {
+//        public int PageNumber { get; set; }
+//        public int PageSize { get; set; }
+//    }
+//}
