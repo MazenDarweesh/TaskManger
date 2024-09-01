cacc1f227b70490aad554e613182be4b0f718380
MazenDarweesh <mazentharwatfathy@gmail.com>
2024-09-01 14:52:32 +0300

    edit handlersutdent

--- Application/Features/Students/Queires/GetStudentByIdQuery.cs
+++ Application/Features/Students/Queires/GetStudentByIdQuery.cs
@@ -1,7 +1,16 @@
 using MediatR;
 using Application.DTOs;
+using Application.IServices;
 
 public class GetStudentByIdQuery : IRequest<StudentDTO>
 {
     public string Id { get; set; }
+}
+public class GetStudentByIdQueryHandler(IStudentService studentService) : IRequestHandler<GetStudentByIdQuery, StudentDTO>
+{
+    private readonly IStudentService _studentService;
+    public async Task<StudentDTO> Handle(GetStudentByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _studentService.GetStudentByIdAsync(request.Id);
+    }
 }
